e33941bd1dee804d5db53ccbd17ec95d0a79e53a
Minojka <stankovic_mina@yahoo.com>
2023-03-30 20:57:08 +0200

    MyInfoTest

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class LoginPage {
-}
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.page.Page;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage extends BasePage{
+
+    @FindBy(name = "username")
+    WebElement userName;
+
+    @FindBy (name = "password")
+    WebElement password;
+
+    @FindBy (css = ".orangehrm-login-button")
+    WebElement loginButton;
+
+    @FindBy(css =".oxd-alert-content-text")
+    WebElement errorMessage;
+
+    public LoginPage(ChromeDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
+    }
+    //akcije
+    public void Login(String name,String pass) {
+        userName.sendKeys(name);
+        password.sendKeys(pass);
+        loginButton.submit();
+    }
+        public String error(){
+            return errorMessage.getText();
+        }
+
+
+    }
+
+
+
+
+
+
--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class BaseTest {
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    public ChromeDriver openBrowser(){
+        ChromeDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//ucitavanje selectora do 10 sec
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));//ucitavanje strane do 10 sec
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/");//u base uvek ide url pocetni
+        return driver;
+    }
 }
